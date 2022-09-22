07dc575dadfb344253feda9e90538beb651de7b2
image_manipulation <felipeutima07@gmail.com>
2022-09-22 18:30:26 -0500

    avance curso

--- prueba.php
+++ prueba.php
@@ -3,6 +3,11 @@
 
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 $fecha = date("d/m/Y");
 $nombre = 'Juan Felipe Tinjaca';
 $edad = 23;
@@ -13,8 +18,12 @@ $peliculas[1] = 'titanic';
 $peliculas[2] = 'dia despues de mañana';
 $peliculas[]= 'otrooo';
 
-
+// comentario  php // 
 ?>
+
+
+
+
 
 <head>
     <meta charset="UTF-8">
@@ -62,6 +71,115 @@ $peliculas[]= 'otrooo';
 
             </tr>
         </table>
+        </div>
+        </div>
+    </div>
+<div class="container">
+    <div class="row">
+    <h1> Array dentro de array (Matriz)</h1>
+    </div>
+
+</div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 py-5 text-align-center">
+
+            <h3>Otra forma es </h3>
+            <?php
+                $aLista=["Arroz", "Carne", "zanahoria"];
+
+                print_r($aLista);
+                ?>
+                <br>
+            <?php
+                $aAgenda = [["Lu", "Ma", "Mi", "Ju", "Vi"],["Libre", "Cursan", "Libre", "Cursan", "Consulta"]];
+
+
+                print_r($aAgenda);
+                ?>
+                <br>
+
+            <h3>Concatenación</h3>
+            <?php
+               $aAuto = array();
+               $aAuto["color"] = array("Negro", "Verde");
+               $aAuto["marca"] = "Ford";
+               $aAuto["anio"] = 1908;
+               $aAuto["precio"] = "USD 800 a USD 1000";
+               
+               echo "El auto " . $aAuto["marca"] . " del año " . $aAuto["anio"] . " es de color " . $aAuto["color"][0] . " y su precio es " . $aAuto["precio"];
+                ?>
+
+            
+
+
+                
+
+
+
+
+
+        </div>
+        </div>
+    </div>
+</div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 py-5 text-align-center">
+
+            <h1>Condicionales</h1>
+            <?php
+              $edad>=18;
+              if ($edad>=18) {
+
+                echo "Es mayor de edad";
+              }
+              else{
+                echo "es menor de edad";
+
+              }
+                ?>
+            <h2>Control de flujo
+            </h2>
+            <?php
+                $stock = 50;
+                $cantidad = '50';
+                if($stock == $cantidad){
+                    echo 'Hay stock'; 
+                }
+                $stock = 50; // I   nteger
+                $cantidad = '“50”'; // String
+                if($stock === $cantidad){
+                    echo 'Hay stock';
+                }
+                var_dump($stock) //para saber el tipo de variable
+                ?>
+            <h3>Valor random</h3>
+            <?php 
+            
+
+            $valor=rand(1,5);
+            
+
+            if ($valor%2==1){
+                echo 'el numero '.$valor. " es impar";
+            }
+            else {
+                echo 'El numero ' .$valor . " es par";
+
+            }
+            ?>
+
+            
+
+             
+
+
+
+
+
         </div>
         </div>
     </div>
